4561a3fe0c5242a409c419637aa1e96cf509889e
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-10-21 10:23:21 +0200

    Added project class and did some work on project view

--- Website/Website/Controllers/HomeController.cs
+++ Website/Website/Controllers/HomeController.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website.Models;
+
 
 namespace Website.Controllers
 {
     public class HomeController : Controller
     {
+        List<Project> ProjectList;
+
+        public HomeController()
+        {
+            ProjectList = new List<Project>();
+
+            ProjectList.Add(new Project()
+            {
+                Title = "HEAJEHSA",
+                Description = "asdasdasdasdasd",
+                Link = "asd"
+            });
+        }
+
         // GET: Home
         public ActionResult Index()
         {
@@ -21,7 +37,8 @@ namespace Website.Controllers
 
         public ActionResult Projects()
         {
-            return View();
+            return View(ProjectList);
+
         }
 
         public ActionResult Contact()
---
+++ Website/Website/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class Project
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
+
